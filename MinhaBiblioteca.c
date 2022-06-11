e941232181287733dbe2a533614b5e0e02d04534
MatheusGustavoSabino <matheusgustavosabino@ucl.br>
2022-06-11 13:05:19 -0300

    MinhaBiblioteca.c Finalizada

--- MinhaBiblioteca.c
+++ MinhaBiblioteca.c
@@ -2,5 +2,14 @@
 #include "MinhaBiblioteca.h"
 
 void pegaMaximoMinimo(int *inteiros, int *maximo, int *minimo, int qtdInteiros){
-    
+    for(int i = 0; i < qtdInteiros; i++)
+  {
+        if(inteiros[i] > *maximo){
+        *maximo = inteiros[i];
+    }
+
+        if(inteiros[i] < *minimo){
+        *minimo = inteiros[i];
+    }
+  }    
 }

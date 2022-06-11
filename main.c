20d963f4294f81544df096d979d00810ed2197b5
MatheusGustavoSabino <matheusgustavosabino@ucl.br>
2022-06-11 12:49:27 -0300

    main concluida

--- main.c
+++ main.c
@@ -5,6 +5,18 @@ int main(void) {
     int qtdInteiros;
     int inteiros[50];
     scanf("%d", &qtdInteiros);
-  
+
+    for(int i = 0; i  qtdInteiros; i++){
+    scanf(%d, &inteiros[i]);
+  }
+
+  int maximo = inteiros[0];
+  int minimo = inteiros[0];
+
+  pegaMaximoMinimo(inteiros, &maximo, &minimo, qtdInteiros);
+
+  for(int i = 0; i  qtdInteiros; i++){
+    printf(%d,, inteiros[i]);
+  }
     printf(nMax=%d, min=%d, maximo, minimo);
 }

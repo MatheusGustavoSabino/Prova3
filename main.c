3cb89aed40630eaf55bc188f5a291ffcc7a7eaf6
MatheusGustavoSabino <matheusgustavosabino@ucl.br>
2022-06-11 12:39:39 -0300

    Criando o esqueleto da main

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "MinhaBiblioteca.h"
+
+int main(void) {
+    int qtdInteiros;
+    int inteiros[50];
+    scanf("%d", &qtdInteiros);
+  
+    printf(nMax=%d, min=%d, maximo, minimo);
+}

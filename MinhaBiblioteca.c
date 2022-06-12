50009841aeb1b29efd646965e8e24590f1afebe1
MatheusGustavoSabino <matheusgustavosabino@ucl.br>
2022-06-12 14:34:55 -0300

    Corrigindo erros na escrita

--- MinhaBiblioteca.c
+++ MinhaBiblioteca.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include "MinhaBiblioteca.h"
 
-void pegaMaximoMinimo(int *inteiros, int *maximo, int *minimo, int qtdInteiros){
-    for(int i = 0; i < qtdInteiros; i++)
-  {
-        if(inteiros[i] > *maximo){
-        *maximo = inteiros[i];
+void pegaMaximoMinimo(int *inteiros, int *maximo, int *minimo, int qtdInteiros) {
+  for(int i = 0; i < qtdInteiros; i++){
+    if(inteiros[i] > *maximo){
+      *maximo = inteiros[i];
     }
 
-        if(inteiros[i] < *minimo){
-        *minimo = inteiros[i];
+    if(inteiros[i] < *minimo){
+      *minimo = inteiros[i];
     }
-  }    
+  }
 }

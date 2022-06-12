befe1f5dce7c078d285710a18b8a343576c7ef91
MatheusGustavoSabino <matheusgustavosabino@ucl.br>
2022-06-12 13:22:30 -0300

    main.c Corrigindo erros de escrita

--- main.c
+++ main.c
@@ -2,12 +2,12 @@
 #include "MinhaBiblioteca.h"
 
 int main(void) {
-    int qtdInteiros;
-    int inteiros[50];
-    scanf("%d", &qtdInteiros);
+  int qtdInteiros;
+  int inteiros[50];
+  scanf("%d", &qtdInteiros);
 
-    for(int i = 0; i  qtdInteiros; i++){
-    scanf(%d, &inteiros[i]);
+  for(int i = 0; i < qtdInteiros; i++){
+    scanf("%d", &inteiros[i]);
   }
 
   int maximo = inteiros[0];
@@ -15,8 +15,8 @@ int main(void) {
 
   pegaMaximoMinimo(inteiros, &maximo, &minimo, qtdInteiros);
 
-  for(int i = 0; i  qtdInteiros; i++){
-    printf(%d,, inteiros[i]);
+  for(int i = 0; i < qtdInteiros; i++){
+    printf("%d,", inteiros[i]);
   }
-    printf(nMax=%d, min=%d, maximo, minimo);
+  printf("\nMax=%d, min=%d", maximo, minimo);
 }
